038282e797fe7d777f1bc1256bbc563c44b892fb
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-06-24 23:32:40 +0200

    test example

--- test/src/decode.js
+++ test/src/decode.js
@@ -49,3 +49,9 @@ test( from_ascii ,
 	null ,
 	'TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4='
 ) ;
+
+test( from_ascii ,
+	'any carnal pleasure' ,
+	null ,
+	'YW55IGNhcm5hbCBwbGVhc3VyZQ=='
+) ;
