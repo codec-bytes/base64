b5fc315ff65380741da1ada7282732945c93fdf5
a flying potato <80830782+a-flying-potato@users.noreply.github.com>
2022-10-16 19:11:05 +0200

    :robot: docs: Use codecov as test link.
    These changes were automatically generated by a transform whose code can be found at:
      - https://github.com/make-github-pseudonymous-again/rejuvenate/blob/3eb37da7d78b30a6f28492864d065b8d8043ab23/src/transforms/docs:use-codecov-as-test-link.js
    Please contact the author of the transform if you believe there was an error.

--- doc/scripts/header.js
+++ doc/scripts/header.js
@@ -17,7 +17,7 @@ domReady(() => {
 	header.insertBefore(projectname, header.firstChild);
 
 	const testlink = document.querySelector('header > a[data-ice="testLink"]');
-	testlink.href = 'https://coveralls.io/github/codec-bytes/base64';
+	testlink.href = 'https://app.codecov.io/gh/codec-bytes/base64';
 	testlink.target = '_BLANK';
 
 	const searchBox = document.querySelector('.search-box');
